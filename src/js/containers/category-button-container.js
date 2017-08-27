bcb89078e37ff77c4773376fc26f01ff1d9be9b3
johnwatkins0 <johnwatkins0@gmail.com>
2017-08-27 15:56:13 -0400

    Debug

--- src/js/containers/category-button-container.js
+++ src/js/containers/category-button-container.js
@@ -4,7 +4,7 @@ import CategoryButton from '../components/category-button';
 const mapStateToProps = (state, ownProps) => ({
   active: state.categories.activeCategory === ownProps.id,
   pages: state.pages.pages[ownProps.id],
-  activePost: state.pages.activePost === null ? null : state.pages.activePost.id,
+  activePost: state.pages.activePost ? state.pages.activePost.id : null,
 });
 
 const CategoryButtonContainer = connect(mapStateToProps)(CategoryButton);
